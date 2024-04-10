af5b45c74c7a6403105e94e3d62a830c89e4352d
Bombardir <bombardirs@hotmail.com>
2024-04-10 22:41:17 +0200

    removed SetNewPortIndex

---
+++ SteamKit2/SteamKit2/Base/SharedArrayMemoryStream.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+using System.IO;
+
+namespace SteamKit2;
+
+public class SharedArrayMemoryStream : MemoryStream
+{
+    /*
+    private const int _arrayLength = 0xFF000;
+    private static readonly ArrayPool<byte> _arrayPool = ArrayPool<byte>.Create( _arrayLength, 8 );
+
+    public SharedArrayMemoryStream() : base( _arrayPool.Rent( _arrayLength ), 0, _arrayLength, true, true)
+    {
+        //SetLength(0);
+    }
+
+    protected override void Dispose( bool disposing )
+    {
+        //_arrayPool.Return( GetBuffer() );
+        base.Dispose( disposing );
+    }*/
+}
